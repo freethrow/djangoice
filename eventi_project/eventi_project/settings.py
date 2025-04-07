ec7d7cfce87485b8b12332ab639c033578af8868
unknown <aleksendric@gmail.com>
2025-04-07 18:42:38 +0200

    fixing deploy

--- eventi_project/eventi_project/settings.py
+++ eventi_project/eventi_project/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+print("setting up eventi_project")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
